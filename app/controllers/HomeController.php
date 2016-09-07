f3d3f3274edd113557707be3100c70b794f00f0b
Eric.Kinch <qi.jin@happysports.com>
2016-09-07 14:49:11 +0800

    add apis.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,14 +15,43 @@ class HomeController extends BaseController
 
 		// $this->view = View::make('home')->with('Currency', Currency::first())->withTitleYes('MFFC :-D')->withFate('OK.');
 		// $Currency = Currency::first();
-		// require dirname(__FILE__). '/../views/home.php';
-		// echo 'home controller.';
-		// $this->slack->channel = ['general', 'random'];
-		// date_default_timezone_set("Asia/Shanghai");
+		echo '<pre>';
+		$client = new \GuzzleHttp\Client();
+		$res = $client->request('GET', 'http://finance.yahoo.com/d/quotes.csv', [
+			'query' => [
+				'e' => '.csv',
+				'f' => 'sl1d1t1',
+				's' => 'USDCNY=X'
+			]
+		]);
+		$result = str_getcsv($res->getBody());
 
-		// $ax = $this->slack->send("时间: ". date("Y-m-d H:i:s"). ", 好的,已经可以完成了.");
+		$datetime = strtotime($result[3]. ' '. $result[2]);
 
-		$this->slack->to("#general")->send(date('Y-m-d H:i:s'). ': New alert. @jinqiqiqi');
-		echo "yes";
+		$time = new DateTime($datetime, new DateTimeZone('UTC'));
+		$time->setTimeZone(new DateTimeZone("Asia/Shanghai"));
+		$format = "Y-m-d H:i:s";
+
+		$fields = [[
+			'title' => 'Currency name',
+			'value' => $result[0],
+			'short' => true
+		],[
+			'title' => 'Value',
+			'value' => $result[1],
+			'short' => true
+		],[
+			'title' => 'Exchange Time',
+			'value' => $time->format($format),
+			'short' => true
+		]];
+		print_r($fields);
+		$this->slack->to("#general")->attach([
+			'fallback' => '当前的交易值',
+			'text' => '当前的交易值',
+			'color' => '#FF0000',
+			'fields' => $fields
+		])->send('当前的交易值:');
+		// $this->slack->send(date('Y-m-d H:i:s'). ': 上午学习时间已经到期. @jinqiqiqi');
 	}
 }
