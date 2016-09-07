b5b8b3c0e5a8ef7132254917940eabd994b08731
qi.jin <qi.jin@happysports.com>
2016-09-07 15:24:33 +0800

    aa

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends BaseController
 		$time->setTimeZone(new DateTimeZone("Asia/Shanghai"));
 		$format = "Y-m-d H:i:s";
 
-		if($result[1] > 6.664) {
+		if($result[1] > 6.7) {
 			$fields = [[
 				'title' => 'Currency name',
 				'value' => $result[0],
